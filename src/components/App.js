033681c3fed1f33a62fc16513097144e7f237e2b
sarahvar <sarahvarin95@gmail.com>
2023-02-05 00:25:40 +0000

    Logements

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
-import './App.css'
+import "./App.css";
 import Header from "./Header.jsx";
+import Banner from "./Layouts/Banner.jsx";
 import Footer from "./Footer.jsx";
-import Banner from './Layouts/Banner.jsx';
+import Logements from "./Layouts/Logements";
+
+
 
 function App() {
- return ( 
+  return (
     <div>
-     <Header />
-     <Banner />
-     <Footer />
-    </div>
- )   
-    
+      <Header />
+      <Banner />
+      <Footer />
+      <Logements />
+   </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layouts/Logements.jsx
@@ -0,0 +1,7 @@
+import "./Logements.css";
+import "../../datas.json/logements.json";
+
+export default function Logements(){
+    return
+}
+
