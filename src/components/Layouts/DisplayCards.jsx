248d91e928860519eeba2b49215666468cfd94e8
sarahvar <sarahvarin95@gmail.com>
2023-02-07 18:12:17 +0000

    début_css_DisplayCards

--- src/components/Layouts/DisplayCards.jsx
+++ src/components/Layouts/DisplayCards.jsx
@@ -1,11 +1,12 @@
 import "./Logements.css";
 import logements from "../../datas/logements.json";
 import Card from "./Card";
+import "../Layouts/DisplayCards.css"
 
 
 export default function displayCards(){
     return(
-    <section>
+    <section className="logements_container">
         {logements.map(({id, title, cover }) => (
         <Card
         key={id}
