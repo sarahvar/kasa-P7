869fcbcd94ab28546162a1bd6911fce47ec370a7
sarahvar <sarahvarin95@gmail.com>
2023-02-15 18:23:25 +0000

    media-error404

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
-import "../pages/error404.css";
 
 export default function ErrorPage() {
   return (
     <div className="error-page">
       <Header />
-      <h1 className="title-error">404</h1>
-      <div className="oups">Oups! La page que vous demandez n'existe pas.</div>
+      <div className="container">
+        <h1 className="title-error">404</h1>
+        <div className="oups">
+          Oups! La page que vous demandez n'existe pas.
+        </div>
         <Link className="link-accueil" to="/">
           retourner sur la page d'accueil
         </Link>
+      </div>
     </div>
   );
 }
