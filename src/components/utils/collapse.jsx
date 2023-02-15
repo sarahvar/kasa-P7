263c3e7012c1d351a9e36380e7f56d3589d03875
sarahvar <sarahvarin95@gmail.com>
2023-02-15 23:28:59 +0000

    collapse

--- src/components/utils/collapse.jsx
+++ src/components/utils/collapse.jsx
@@ -1,22 +1,18 @@
-import React,{useState} from 'react';
-
-
-
+import React, { useState } from "react";
 
 const Collapsible = (props) => {
-    const [open, setOPen] = useState(false);
-    const toggle = () => {
-        setOPen(!open);
-      };
-      
-    return (
-        <div>
-          <button className='button' onClick={toggle}>{props.label}</button>
-          {open && (
-            <p className="text">
-            {props.content}
-            </p>
-          )}
-        </div>
-  )};
+  const [open, setOPen] = useState(false);
+  const toggle = () => {
+    setOPen(!open);
+  };
+
+  return (
+    <div>
+      <button className="button" onClick={toggle}>
+        {props.label}
+      </button>
+      {open && <p className="text">{props.content}</p>}
+    </div>
+  );
+};
 export default Collapsible;
