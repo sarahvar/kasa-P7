ebe534297095032ab666b22bc171fd4582643b31
sarahvar <sarahvarin95@gmail.com>
2023-02-16 18:05:15 +0000

    chevrons

--- src/components/utils/collapse.jsx
+++ src/components/utils/collapse.jsx
@@ -10,6 +10,15 @@ const Collapsible = (props) => {
     <div>
       <button className="button" onClick={toggle}>
         {props.label}
+        {!open ? (
+					<span>
+						<i class="fa-solid fa-chevron-down"></i>
+					</span>
+				) : (
+					<span>
+						<i class="fa-solid fa-chevron-up"></i>
+					</span>
+				)}
       </button>
       {open && <p className="text">{props.content}</p>}
     </div>
